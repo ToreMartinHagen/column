f0174d14068eb0fa6302912c8743c04903b45af3
Tore Martin Hagen <tore@instanes.name>
2020-10-04 20:13:06 +0200

    Added simple implementation of splitNewlines

--- column.py
+++ column.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-def splitNewlines(string, colomnWidth):
-    return "per\nole"
+def splitNewlines(line, colomnWidth):
+    index = colomnWidth
+    index = line.rfind(' ', index)
+    return line[:index] + "\n" + line[index+1:]
 
 text="per ole"
 res = splitNewlines(text, 3)
+#print(res)
 assert res == "per\nole"
-
