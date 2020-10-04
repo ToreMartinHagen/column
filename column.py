5007d869b7daeabf24104e421028d36be9eb0ea6
Tore Martin Hagen <tore@instanes.name>
2020-10-04 21:37:05 +0200

    Fixed but not wrapping filling of spaces

--- column.py
+++ column.py
@@ -11,12 +11,17 @@ def splitNewlines(line, colomnWidth):
     return line
 
 def addSpacesInLine(line, colomnWidth):
-    index = 0    
-    while(len(line) < colomnWidth and index >= 0):
+    index = 0
+    if(line.find(' ', index) < 0):
+       return line
+       
+    while(len(line) < colomnWidth):
         index = line.find(' ', index)
         if(index >= 0):
             line = line[:index] + " " + line[index:]
-            index += 1
+            index += 2
+        else:
+            index = 0        
     return line
 
 def addSpaces(lines, colomnWidth):
@@ -72,6 +77,16 @@ text="xx yy zz"
 res = makeColumns(text, 6)
 #print(res)
 assert res == "xx  yy\nzz\n"
+
+text="xx yy zz"
+res = makeColumns(text, 10)
+#print(res)
+assert res == "xx  yy  zz\n"
+
+#text="reporters on Saturday was"
+#res = addSpacesInLine(text, 30)
+#print("0123456789 123456789 123456789")
+#print(res)
 
 text="Conley also said that Chief of Staff Mark Meadows' update to reporters on Saturday was misconstrued, adding that the chief and I work side by side. The president's vitals over the last 24 hours were very concerning and the next 48 hours will be critical in terms of his care. We're still not on a clear path to a full recovery, Meadows said. The doctor confirmed that Meadows was referencing the president's high fever and oxygen drop 24 hours earlier. Trump shared a video message from hospital on Saturday evening, warning the next few days will be the real test in his fight against the killer bug."
 
