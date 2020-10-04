8a222adb6e9c12bc470487ae466e51603302785b
Tore Martin Hagen <tore@instanes.name>
2020-10-04 20:21:54 +0200

    Fixed bug in rfind

--- column.py
+++ column.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def splitNewlines(line, colomnWidth):
-    index = colomnWidth
-    index = line.rfind(' ', index)
+    index = colomnWidth + 1
+    index = line.rfind(' ', 0, index)
     return line[:index] + "\n" + line[index+1:]
 
 text="per ole"
 res = splitNewlines(text, 3)
 #print(res)
 assert res == "per\nole"
+
